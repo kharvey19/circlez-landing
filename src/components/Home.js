7be5a86f22d261403fc6350a2558004e83ce2dcf
Katherine Harvey <kharvey@college.harvard.edu>
2023-06-29 17:39:49 -0400

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <main className="bg-black w-screen">
     <p className="text-white text-6xl md:inter font-bold pt-44 text-center "> A <span className='text-indigo-400'> hub </span> for motivation.</p>
       <div className="md:grid md:grid-cols-2 ">
-        <div className=" mr-10 md:mr-0 md:ml-40 md:pt-20 col-span-1 mb-36 pt-16">
+        <div className=" mr-10 md:mr-0 md:ml-40 md:pt-20 col-span-1 mb-20 md:mb-36 pt-16">
             <p className="ml-12 text-white text-center md:text-left md:mt-36 text-3xl md:text-4xl font-bold">A social media app startup harnessing the power of positive peer pressure to help young adults track and achieve goals.</p>
             <p className="ml-12 text-zinc-500 text-center md:text-left text-2xl mt-5">Join our first 100 user list to get premium access to Circlez when it launches.</p>
             <div className='ml-12 flex justify-center'>
