7de4ffd5db2e2bbf52dc9ed95c3d296e8f7aab4c
Katherine Harvey <kharvey@college.harvard.edu>
2023-06-30 17:29:25 -0400

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className="md:grid md:grid-cols-2">
             <div className="md:ml-20 pt-20 ">
                 <h1 className="text-5xl md:text-6xl mb-6 font-bold text-white text-center md:text-left md:inter">Contact Us</h1>
-                <p className="text-md mb-6 text-white md:inter text-left text-zinc-500 mr-10 ml-10 md:ml-0">
+                <p className="text-md mb-6 text-white md:inter text-left text-zinc-500 mr-36 ml-10 md:ml-0">
                     We are always looking for new opportunities to partner with organizations and individuals who share our vision. If you are interested in working with us, please reach out! </p>
                    
             </div> 
