fe8db2fda09b5f932980a5ddb6fd0f12f1456e15
cfharvard <112586108+cfharvard@users.noreply.github.com>
2023-09-07 20:20:43 -0400

    added linkedIn and IG icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ContactForm from './ContactForm';
+import IconButton from '@mui/material/IconButton';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Contact = () => {
   return (
-    <div className="text-white  bg-black b ">
+    <div className="text-white  bg-black">
         <div className="md:grid md:grid-cols-2">
-            <div className="md:ml-20 pt-20 ">
+            <div className="flex flex-col items-center justify-center md:ml-20 pt-20 md:flex-none md:items-start">
                 <h1 className="text-5xl md:text-6xl mb-6 font-bold text-white text-center md:text-left md:inter">Contact Us</h1>
-                <p className="text-md mb-6 text-white md:inter text-left text-zinc-500 mr-36 ml-10 md:ml-0">
-                    We are always looking for new opportunities to partner with organizations and individuals who share our vision. If you are interested in working with us, please reach out! </p>
-                   
+                <p className="text-md text-center md:text-left md:inter text-zinc-500 md:mr-36 md:ml-0">
+                    We are always looking for new opportunities to partner with organizations and individuals who share our vision. 
+                    If you are interested in working with us, please reach out! </p>
+                <div className="pt-4 m-0 w-fit">
+                    <IconButton href="https://instagram.com/circlez_official?igshid=MzRlODBiNWFlZA==" target="_blank" style={{paddingLeft: 0}}><InstagramIcon style={{ color: "#FFF",  fontSize: "50px" }}/></IconButton>
+                    <IconButton href="https://www.linkedin.com/company/circlez-llc/" target="_blank" style={{ paddingRight: 0 }}><LinkedInIcon style={{ color: "#FFF", fontSize: "50px" }}/></IconButton>
+                </div>
             </div> 
-     <ContactForm />
-            </div>
-            </div>                 
+            <ContactForm />
+        </div>
+      </div>                 
 
   );
 };
